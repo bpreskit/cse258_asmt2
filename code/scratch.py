66cd0be57641caadfbf2269cc60a45363d24bec5
Brian Preskitt <bppresk@yahoo.com>
2017-03-11 15:03:37 -0800

    Scratch works functionally with shuffle

--- code/scratch.py
+++ code/scratch.py
@@ -14,13 +14,13 @@ shuf_fil = dat_dir + 'shuf_flights.csv'
 airlines = pd.read_csv(dat_dir + 'airlines.csv')
 airports = pd.read_csv(dat_dir + 'airports.csv')
 
+print('Shuffling and reading')
 num_flights = 100000
 with open(raw_fil, 'r') as file:
     header = file.readline()
 with open(shuf_fil, 'w') as file:
     file.write(header)
-os.system('shuf -n {0} {1}flights.csv >> '
-          '{2}shuf_flights.csv'.format(num_flights, dat_dir, dat_dir))
+os.system('shuf -n {0} {1} >> {2}'.format(num_flights, raw_fil, shuf_fil))
 flights = pd.read_csv(dat_dir + '/shuf_flights.csv', low_memory=False)
 os.remove(dat_dir + 'shuf_flights.csv')
 
@@ -31,15 +31,24 @@ flights['AIRLINE_desc'] = flights['AIRLINE'].apply(lambda a: airlines_dict[a])
 
 flights = flights[flights['DEPARTURE_DELAY'].notnull() & flights['ARRIVAL_DELAY'].notnull()]
 
-delays = flights['ARRIVAL_DELAY'].dropna()
+delays = flights['ARRIVAL_DELAY']
 
 print('Getting bins')
+# 4 quartiles of delays, used to show color-coded hist of aggregated
+# delay data
 q_labels, bins = pd.qcut(delays, 4, retbins=True)
 hbins = list(bins) + list(np.linspace(delays.min(), delays.max(), 200))
 hbins.sort()
-
-#pdb.set_trace()
 quarts = delays.groupby(q_labels)
+
+# Time bins, what time did flights occur.  Used to identify traffic jams
+t_midnight = 2400
+t_step = 300
+t_bins = np.arange(0, t_midnight + t_step, t_step)
+t_labels = pd.cut(flights['SCHEDULED_DEPARTURE'], t_bins)
+
+# Delay bins, used to visualize what effect other variables may have
+h_labels = pd.qcut(delays, 10)
 
 print('Trying to plot')
 fig = plt.figure()
@@ -57,12 +66,7 @@ sns.distplot((flights['ARRIVAL_DELAY'] - flights['DEPARTURE_DELAY']).dropna(), k
 plt.title('Histogram of Arr. Delay - Dep. Delay')
 fig.show()
 
-t_step = 300
-t_midnight = 2400
-t_bins = list(np.arange(0, t_midnight + t_step, t_step))
-h_labels = pd.qcut(flights['ARRIVAL_DELAY'], 10)
-t_labels = pd.cut(flights['SCHEDULED_DEPARTURE'], t_bins)
-# hmap_delay_airline = flights.pivot_table(values=h_labels, index=h_labels, columns=['AIRLINE'], aggfunc=lambda x: x.count())
+hmap_delay_airline = flights.pivot_table(values=h_labels, index=h_labels, columns=['AIRLINE'], aggfunc=lambda x: x.count())
 
 fig = plt.figure()
 hmap_del_airline = pd.crosstab(index=h_labels,
@@ -72,13 +76,13 @@ ax = sns.heatmap(hmap_del_airline, linewidths=0.5)
 plt.setp(ax.get_yticklabels(), rotation=0)
 fig.show()
 
-fig = plt.figure()
-hmap_del_month = pd.crosstab(index=h_labels,
-                             columns=flights['AIRLINES'],
-                             normalize='columns')
-ax = sns.heatmap(hmap_del_month, linewidths=0.5)
-plt.setp(ax.get_yticklabels(), rotation=0)
-fig.show()
+# fig = plt.figure()
+# hmap_del_month = pd.crosstab(index=h_labels,
+#                              columns=flights['AIRLINES'],
+#                              normalize='columns')
+# ax = sns.heatmap(hmap_del_month, linewidths=0.5)
+# plt.setp(ax.get_yticklabels(), rotation=0)
+# fig.show()
 
 fig = plt.figure()
 hmap_del_dow = pd.crosstab(index=h_labels,
