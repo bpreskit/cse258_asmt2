83f5258fbd5ea587bd4d3894c793de5137033a30
Brian Preskitt <bppresk@yahoo.com>
2017-03-11 13:59:35 -0800

    shuffling working properly

--- code/scratch.py
+++ code/scratch.py
@@ -6,6 +6,8 @@ import seaborn as sns
 import pdb
 
 dat_dir = 'local_data/'
+raw_fil = dat_dir + 'flights.csv'
+shuf_fil = dat_dir + 'shuf_flights.csv'
 
 # In[3]:
 
@@ -13,7 +15,11 @@ airlines = pd.read_csv(dat_dir + 'airlines.csv')
 airports = pd.read_csv(dat_dir + 'airports.csv')
 
 num_flights = 100000
-os.system('shuf -n {0} {1}flights.csv > '
+with open(raw_fil, 'r') as file:
+    header = file.readline()
+with open(shuf_fil, 'w') as file:
+    file.write(header)
+os.system('shuf -n {0} {1}flights.csv >> '
           '{2}shuf_flights.csv'.format(num_flights, dat_dir, dat_dir))
 flights = pd.read_csv(dat_dir + '/shuf_flights.csv', low_memory=False)
 os.remove(dat_dir + 'shuf_flights.csv')
@@ -51,8 +57,12 @@ sns.distplot((flights['ARRIVAL_DELAY'] - flights['DEPARTURE_DELAY']).dropna(), k
 plt.title('Histogram of Arr. Delay - Dep. Delay')
 fig.show()
 
-h_labels = pd.qcut(delays, 10)
-hmap_delay_airline = flights.pivot_table(values=h_labels, index=h_labels, columns=['AIRLINE'], aggfunc=lambda x: x.count())
+t_step = 300
+t_midnight = 2400
+t_bins = list(np.arange(0, t_midnight + t_step, t_step))
+h_labels = pd.qcut(flights['ARRIVAL_DELAY'], 10)
+t_labels = pd.cut(flights['SCHEDULED_DEPARTURE'], t_bins)
+# hmap_delay_airline = flights.pivot_table(values=h_labels, index=h_labels, columns=['AIRLINE'], aggfunc=lambda x: x.count())
 
 fig = plt.figure()
 hmap_del_airline = pd.crosstab(index=h_labels,
@@ -62,18 +72,26 @@ ax = sns.heatmap(hmap_del_airline, linewidths=0.5)
 plt.setp(ax.get_yticklabels(), rotation=0)
 fig.show()
 
-# fig = plt.figure()
-# hmap_del_month = pd.crosstab(index=h_labels,
-#                              columns=flights['AIRLINES'],
-#                              normalize='columns')
-# ax = sns.heatmap(hmap_del_month, linewidths=0.5)
-# plt.setp(ax.get_yticklabels(), rotation=0)
-# fig.show()
+fig = plt.figure()
+hmap_del_month = pd.crosstab(index=h_labels,
+                             columns=flights['AIRLINES'],
+                             normalize='columns')
+ax = sns.heatmap(hmap_del_month, linewidths=0.5)
+plt.setp(ax.get_yticklabels(), rotation=0)
+fig.show()
 
 fig = plt.figure()
 hmap_del_dow = pd.crosstab(index=h_labels,
                            columns=flights['DAY_OF_WEEK'],
                            normalize='columns')
 ax = sns.heatmap(hmap_del_dow, linewidths=0.5)
+plt.setp(ax.get_yticklabels(), rotation=0)
+fig.show()
+
+fig = plt.figure()
+hmap_del_time = pd.crosstab(index=h_labels,
+                            columns=t_labels,
+                            normalize='columns')
+ax = sns.heatmap(hmap_del_time, linewidths=0.5)
 plt.setp(ax.get_yticklabels(), rotation=0)
 fig.show()
